a1993936108bf5bbbe3eaaead1b3e953ce56bcbc
Pierangelo Castillo <ilgato@users.noreply.github.com>
2017-06-14 13:12:55 +0200

    1st test
    added a test page with css

---
+++ wisdom-webdisplay/src/main/java/impl/WDisplayController.java
@@ -0,0 +1,24 @@
+package impl;
+
+import org.wisdom.api.DefaultController;
+import org.wisdom.api.annotations.Controller;
+import org.wisdom.api.annotations.Route;
+import org.wisdom.api.annotations.View;
+import org.wisdom.api.http.HttpMethod;
+import org.wisdom.api.http.Result;
+import org.wisdom.api.templates.Template;
+
+
+@Controller
+public class WDisplayController extends DefaultController {
+
+
+    @View("status")
+    Template status;
+
+    @Route(method = HttpMethod.GET, uri = "/status")
+    public Result status() {
+        return ok(render(status));
+    }
+
+}
